025385c8ed98408ed4545b07d418f5dd130a67b5
PolMauri5 <polmauri5@gmail.com>
2025-09-22 20:14:17 +0200

    feat: add EN and ES validation messages

---
+++ resources/lang/es/message.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'field_prefix' => 'El campo :field',
+    'rules' => [
+        'required' => 'es obligatorio',
+        'email' => 'debe ser un correo electrónico válido',
+        'min' => 'debe tener al menos :min caracteres',
+    ],
+    'and' => 'y',
+];
